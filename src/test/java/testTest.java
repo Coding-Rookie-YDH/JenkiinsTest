9fa43f521aa94406b48d6eb92553afbc15f128a1
Administrator <2248264768@qq.com>
2020-11-29 00:37:51 +0800

    222

--- src/test/java/testTest.java
+++ src/test/java/testTest.java
@@ -1,8 +1,10 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import org.mockito.Mockito;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class testTest {
 
